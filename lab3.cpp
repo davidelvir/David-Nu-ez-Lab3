aaa019566e6d5b70e642be505890de4a46349038
David <david_elvir@unitec.edu>
2017-05-05 13:25:27 -0600

    Comienzo del proyecto

---
+++ lab3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int opcion;
+  cout<<"------Menu------"<<endl;
+  cout<<"1. Numeros malvados."<<endl
+      <<"2. Rotar matriz.";
+  cin>>opcion;
+}
